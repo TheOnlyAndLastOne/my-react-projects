230f9f1284a436f94d229a7062ae90f973873926
peter.zhao <peter.zhao@vechain.com>
2022-02-20 19:42:53 +0800

    init tailwind and ant design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
+import 'antd/dist/antd.css';
+// import 'antd/dist/antd.less';
+import {Button} from 'antd';
 function App() {
   return (
     <div className="App">
       <h1 className="text-blue-700">Hello Wolrd!</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
