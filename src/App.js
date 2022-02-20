790c4a14ce1ea73c1a2d7abed1468d348de46761
peter.zhao <peter.zhao@vechain.com>
2022-02-20 19:26:25 +0800

    ant-design-empty

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
+import 'antd/dist/antd.css'
+import { Button } from 'antd';
+import MyButton from './components/Button/MyButton'
+
 function App() {
   return (
     <div className="App">
       <h1>Hello Wolrd!</h1>
+      <MyButton/>
     </div>
   );
 }
---
+++ src/components/Button/MyButton.js
@@ -0,0 +1,16 @@
+import { Button } from "antd";
+
+function MyButton() {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </div>
+  );
+}
+
+export default MyButton;
