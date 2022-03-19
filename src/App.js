fd686dccf6590b1512218fd36b68b711945429c5
peter.zhao <peter.zhao@vechain.com>
2022-03-19 10:37:52 +0800

    add proxy config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import "antd/dist/antd.min.css";
+import { Upload, message, Button } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
-import 'antd/dist/antd.css'
-import { Button } from 'antd';
-import MyButton from './components/Button/MyButton'
+const props = {
+  name: "file",
+  action: "api/upload",
+  headers: {
+    authorization: "authorization-text",
+  },
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Wolrd!</h1>
-      <MyButton/>
+      <h1>Hello World!</h1>
+      <Upload {...props} multiple directory>
+        <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload>
     </div>
   );
 }
