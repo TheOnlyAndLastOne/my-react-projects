580d0805ef053312500ebce60d8f9195b0e0f303
peter.zhao <peter.zhao@vechain.com>
2022-03-31 20:05:32 +0800

    add material and tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-blue-700">Hello Wolrd!</h1>
+      <Button variant="contained">Hello Wolrd!</Button>
     </div>
   );
 }
