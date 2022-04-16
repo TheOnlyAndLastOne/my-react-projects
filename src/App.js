3f22c4ce91831684cd865155b87ab5d4891e4bfc
zhi.zhao <zhi.zhao@taotu-partner.com>
2022-04-16 14:12:20 +0800

    install daisyui and theme-change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 
 function App() {
+  useEffect(() => {
+    themeChange(false);
+    // 👆 false parameter is required for react project
+  }, []);
+
   return (
     <div className="App">
       <h1 className="text-blue-700">Hello Wolrd!</h1>
+      <div>
+        <button className="btn btn-primary">button</button>
+        666
+      </div>
+      <div>
+        <select data-choose-theme>
+          <option value="">Default</option>
+          <option value="dark">dark</option>
+          <option value="garden">garden</option>
+        </select>
+      </div>
     </div>
   );
 }
